71478c5e2d0c35b64b4612b6c8a972c9c9d9c23d
JeppeSigaard <jeppe@smatmonkey.dk>
2016-04-25 12:58:26 +0200

    smamo_inner_img in og-meta

--- libs/og-meta.php
+++ libs/og-meta.php
@@ -4,6 +4,23 @@
 Opret bedre meta tags
 */
 
+if (!function_exists('smamo_inner_img')){
+    function smamo_inner_img($post_id = false) {
+        global $post, $posts;
+        if ($post_id){$post = get_post($post_id);}
+        $first_img = '';
+        
+        $output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $post->post_content, $matches);
+        if (isset($matches[1]) && isset($matches[1][0])){
+            $first_img = $matches[1][0];    
+        }
+
+        if(empty($first_img)) {$first_img = false;}
+        
+        return $first_img;
+    }
+}
+
 add_action('wp_head', 'smamo_add_metas');
 
 function smamo_add_metas(){
@@ -16,6 +33,10 @@ function smamo_add_metas(){
     
     if ($image_url && isset($image_url[0])){
         $meta_img = $image_url[0];
+    }
+    
+    if (!$meta_img){
+        $meta_img = smamo_inner_img($post->ID);
     }
     
     if (!$meta_img || is_home() || is_front_page()){
